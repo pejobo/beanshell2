37f47abe612ad043cec8571aafe3a3f0656a5768
fschmidt <fschmidt@8af74f78-9a2f-0410-974c-5db699c9257e>
2008-03-29 07:14:12 +0000

    fix issue #18 - Wrong static method called

--- trunk/src/bsh/Reflect.java
+++ trunk/src/bsh/Reflect.java
@@ -522,6 +522,12 @@ class Reflect
 		Class baseClass, String methodName, int numArgs,
 		List<Method> publicMethods, List<Method> nonPublicMethods )
 	{
+		// Do we have a superclass? (interfaces don't, etc.)
+		Class superclass = baseClass.getSuperclass();
+		if ( superclass != null )
+			gatherMethodsRecursive( superclass,
+				methodName, numArgs, publicMethods, nonPublicMethods );
+
 		// Add methods of the current class to the list.
 		// In public case be careful to only add methods from a public class
 		// and to use getMethods() instead of getDeclaredMethods()
@@ -544,15 +550,8 @@ class Reflect
 		}
 
 		// Does the class or interface implement interfaces?
-		Class [] intfs = baseClass.getInterfaces();
-		for( int i=0; i< intfs.length; i++ )
-			gatherMethodsRecursive(  intfs[i],
-				methodName, numArgs, publicMethods, nonPublicMethods );
-
-		// Do we have a superclass? (interfaces don't, etc.)
-		Class superclass = baseClass.getSuperclass();
-		if ( superclass != null )
-			gatherMethodsRecursive( superclass,
+		for( Class intf : baseClass.getInterfaces() )
+			gatherMethodsRecursive(  intf,
 				methodName, numArgs, publicMethods, nonPublicMethods );
 	}
 
