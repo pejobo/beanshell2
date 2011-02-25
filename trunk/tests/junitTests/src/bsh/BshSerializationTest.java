baa8823d5342f57422f2fbd33d71765a133c9cc2
pejobo70@gmail.com <pejobo70@gmail.com@8af74f78-9a2f-0410-974c-5db699c9257e>
2011-02-25 14:16:48 +0000

    fixes Issue 35 CallStack class should be Serializable
    * added "implements java.io.Serializable"
    * added test class
    * added test util class

--- trunk/tests/junitTests/src/bsh/BshSerializationTest.java
+++ trunk/tests/junitTests/src/bsh/BshSerializationTest.java
@@ -1,11 +1,5 @@
 package bsh;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,47 +9,31 @@ import org.junit.Test;
  * @author Jessen Yu
  *
  */
-public class BshSerializationTest
-{
-    /**
-     * Serializes and then deserializes the given Interpreter.
-     * 
-     * @param origInterpreter
-     * @return
-     * @throws IOException
-     * @throws ClassNotFoundException
-     */
-    private Interpreter serDeser(Interpreter origInterpreter) throws IOException, ClassNotFoundException
-    {
-        ByteArrayOutputStream byteOS = new ByteArrayOutputStream();
-        new ObjectOutputStream(byteOS).writeObject(origInterpreter);
-        return (Interpreter) new ObjectInputStream(
-                new ByteArrayInputStream(byteOS.toByteArray())).readObject();
-    }
-    
+public class BshSerializationTest {
+
     /**
      * Tests that Special.NULL_VALUE is correctly serialized/deserialized
+	 * @throws Exception in case of failure
      */
     @Test
-    public void testNullValueSerialization() throws Throwable
-    {
-        Interpreter origInterpreter = new Interpreter();
+    public void testNullValueSerialization() throws Exception {
+		final Interpreter origInterpreter = new Interpreter();
         origInterpreter.eval("myNull = null;");
-        Assert.assertEquals(null, origInterpreter.eval("myNull"));
-        Interpreter deserInterpreter = serDeser(origInterpreter);
-        Assert.assertEquals(null, deserInterpreter.eval("myNull"));
+		Assert.assertNull(origInterpreter.eval("myNull"));
+        final Interpreter deserInterpreter = TestUtil.serDeser(origInterpreter);
+		Assert.assertNull(deserInterpreter.eval("myNull"));
     }
 
     /**
      * Tests that Primitive.NULL is correctly serialized/deserialized
-     */
+	 * @throws Exception in case of failure
+	 */
     @Test
-    public void testSpecialNullSerialization() throws Throwable
-    {
-        Interpreter originalInterpreter = new Interpreter();
+    public void testSpecialNullSerialization() throws Exception {
+		final Interpreter originalInterpreter = new Interpreter();
         originalInterpreter.eval("myNull = null;");
-        Assert.assertTrue(((Boolean)originalInterpreter.eval("myNull == null")).booleanValue());
-        Interpreter deserInterpreter = serDeser(originalInterpreter);
-        Assert.assertTrue(((Boolean)deserInterpreter.eval("myNull == null")).booleanValue());
+        Assert.assertTrue((Boolean) originalInterpreter.eval("myNull == null"));
+        final Interpreter deserInterpreter = TestUtil.serDeser(originalInterpreter);
+        Assert.assertTrue((Boolean) deserInterpreter.eval("myNull == null"));
     }
 }
