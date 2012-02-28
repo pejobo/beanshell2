b1297431af0da5636e3c801bf80a4f65a53be45e
pejobo70@googlemail.com <pejobo70@googlemail.com@8af74f78-9a2f-0410-974c-5db699c9257e>
2012-02-28 17:03:06 +0000

    fixes issue#67

--- trunk/tests/junitTests/src/bsh/Issue_55_Test.java
+++ trunk/tests/junitTests/src/bsh/Issue_55_Test.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import javax.script.ScriptException;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 
 @RunWith(FilteredTestRunner.class)
 public class Issue_55_Test {
@@ -27,6 +30,18 @@ public class Issue_55_Test {
 		externalNameSpace.setVariable("a", Primitive.NULL, false);
 		assertTrue("map should contain variable 'a'", externalNameSpace.getMap().containsKey("a"));
 		assertNull("variable 'a' should have value <NULL>", externalNameSpace.getMap().get("a"));
+	}
+
+	@Category( NotSuitedFor_Java5_OrLower.class )
+	@Test
+	public void issue_67() throws Exception {
+		final String script = "print(\"test\";";
+		try {
+			new BshScriptEngineFactory().getScriptEngine().eval(script);
+			fail("expected script exception");
+		} catch (ScriptException e) {
+			assertEquals(1, e.getLineNumber());
+		}
 	}
 
 }
