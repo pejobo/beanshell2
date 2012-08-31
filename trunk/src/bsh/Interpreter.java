a20f919938de965543c2f229c381776353c5e62b
pejobo70@gmail.com <pejobo70@gmail.com@8af74f78-9a2f-0410-974c-5db699c9257e>
2012-08-31 12:52:41 +0000

    Issue 72
    2.2b0 -> 2.2.0

--- trunk/src/bsh/Interpreter.java
+++ trunk/src/bsh/Interpreter.java
@@ -107,7 +107,7 @@ public class Interpreter
 {
 	/* --- Begin static members --- */
 
-	public static final String VERSION = "2.2b0";
+	public static final String VERSION = "2.2.0";
 	/*
 		Debug utils are static so that they are reachable by code that doesn't
 		necessarily have an interpreter reference (e.g. tracing in utils).
